e44e41c24c2bbbb497a63d4eda8d4fe80795ed17
ahmedmsaid <dyabco74@gmail.com>
2022-12-12 19:44:40 +0200

    Some Validation Added

--- XO_Game/Program.cs
+++ XO_Game/Program.cs
@@ -8,7 +8,7 @@
 
             int input, n = 9;
 
-            XO game= new XO();
+            XO game = new XO();
 
             Console.WriteLine("Welcome");
             game.GenerateTable();
@@ -17,141 +17,57 @@
             {
                 Console.Write("Enter X or O: ");
                 char.TryParse(Console.ReadLine(), out XorO);
+
+                while (!game.IsDecisionCorrect(XorO))
+                {
+                    Console.WriteLine("Invalid, Enter only X or O");
+                    char.TryParse(Console.ReadLine(), out XorO);
+
+                }
+
                 game.SetDecision(XorO);
 
                 Console.WriteLine();
 
                 Console.Write("Enter a place number: ");
                 int.TryParse(Console.ReadLine(), out input);
+
+                while (game.IsPlaceAlreadyChecked(input))
+                {
+                    Console.WriteLine("Already Checked, Enter a place number: ");
+                    int.TryParse(Console.ReadLine(), out input);
+                }
+
                 game.SetKey(input);
 
                 game.ChangeTable();
+
                 game.GenerateTable();
 
-                if(game.IsXWinner())
+                Console.WriteLine();
+
+                if (game.IsXWinner())
                 {
+                    Console.ForegroundColor= ConsoleColor.Green;
                     Console.WriteLine("X wins");
                     break;
                 }
 
                 if (game.IsOWinner())
                 {
+                    Console.ForegroundColor= ConsoleColor.Green;
                     Console.WriteLine("O wins");
                     break;
                 }
 
                 n--;
             }
-        }
-    }
 
-    public class XO {
-        private int key;
-
-        private char[] _decisions = { 'X', 'O' };
-
-        private char _decision;
-
-        private char[,] _table =
-        {
-            {'1', '2', '3'},
-            {'4', '5', '6'},
-            {'7', '8', '9'}
-        };
-
-        //public char this[int row, int col]
-        //{
-        //    get
-        //    {
-        //        return _table[row, col];
-        //    }
-        //    set
-        //    {
-        //        _table[row, col] = value;
-        //    }
-        //}
-
-        public void GenerateTable()
-        {
-            Console.WriteLine($"{_table[0, 0]} | {_table[0, 1]} | {_table[0, 2]}");
-            Console.WriteLine("---------");
-            Console.WriteLine($"{_table[1, 0]} | {_table[1, 1]} | {_table[1, 2]}");
-            Console.WriteLine("---------");
-            Console.WriteLine($"{_table[2, 0]} | {_table[2, 1]} | {_table[2, 2]}");
-        }
-
-        public void SetDecision(char decision)
-        {
-            if (decision == 'X') _decision = _decisions[0];
-            else if (decision == 'O') _decision = _decisions[1];
-        }
-
-        public void SetKey(int key)
-        {
-            this.key = key;
-        }
-
-        public void ChangeTable()
-        {
-            switch(this.key)
+            if (game.IsXWinner() == false && game.IsOWinner() == false)
             {
-                case 1:
-                    _table[0, 0] = _decision;
-                    break;
-                case 2:
-                    _table[0, 1] = _decision;
-                    break;
-                case 3:
-                    _table[0, 2] = _decision;
-                    break;
-                case 4:
-                    _table[1, 0] = _decision;
-                    break;
-                case 5:
-                    _table[1, 1] = _decision;
-                    break;
-                case 6:
-                    _table[1, 2] = _decision;
-                    break;
-                case 7:
-                    _table[2, 0] = _decision;
-                    break;
-                case 8:
-                    _table[2, 1] = _decision;
-                    break;
-                case 9:
-                    _table[2, 2] = _decision;
-                    break;
-                default:
-                    Console.WriteLine();
-                    break;
-
+                Console.WriteLine("\nTIE");
             }
-        }
 
-        public bool IsXWinner()
-        {
-            if ((_table[0, 0] == 'X' && _table[1, 0] == 'X' && _table[2, 0] == 'X') ||
-                (_table[0, 0] == 'X' && _table[0, 1] == 'X' && _table[0, 2] == 'X') ||
-                (_table[0, 0] == 'X' && _table[1, 1] == 'X' && _table[2, 2] == 'X') ||
-                (_table[1, 0] == 'X' && _table[1, 1] == 'X' && _table[1, 2] == 'X') ||
-                (_table[2, 0] == 'X' && _table[2, 1] == 'X' && _table[2, 2] == 'X') ||
-                (_table[0, 1] == 'X' && _table[1, 1] == 'X' && _table[2, 1] == 'X') ||
-                (_table[0, 2] == 'X' && _table[1, 2] == 'X' && _table[2, 2] == 'X'))
-                return true;
-            return false;
-        }
-        public bool IsOWinner()
-        {
-            if ((_table[0, 0] == 'O' && _table[1, 0] == 'O' && _table[2, 0] == 'O') ||
-                (_table[0, 0] == 'O' && _table[0, 1] == 'O' && _table[0, 2] == 'O') ||
-                (_table[0, 0] == 'O' && _table[1, 1] == 'O' && _table[2, 2] == 'O') ||
-                (_table[1, 0] == 'O' && _table[1, 1] == 'O' && _table[1, 2] == 'O') ||
-                (_table[2, 0] == 'O' && _table[2, 1] == 'O' && _table[2, 2] == 'O') ||
-                (_table[0, 1] == 'O' && _table[1, 1] == 'O' && _table[2, 1] == 'O') ||
-                (_table[0, 2] == 'O' && _table[1, 2] == 'O' && _table[2, 2] == 'O'))
-                return true;
-            return false;
         }
     }
 }
---
+++ XO_Game/XO.cs
@@ -0,0 +1,133 @@
+﻿namespace XO_Game
+{
+    public class XO
+    {
+        private int key;
+
+        private char[] decisions = { 'X', 'O' };
+
+        private char decision;
+
+        private char[,] table =
+        {
+            {'1', '2', '3'},
+            {'4', '5', '6'},
+            {'7', '8', '9'}
+        };
+
+        //public char this[int row, int col]
+        //{
+        //    get
+        //    {
+        //        return _table[row, col];
+        //    }
+        //    set
+        //    {
+        //        _table[row, col] = value;
+        //    }
+        //}
+
+        public void GenerateTable()
+        {
+            Console.WriteLine($"{table[0, 0]} | {table[0, 1]} | {table[0, 2]}");
+            Console.WriteLine("---------");
+            Console.WriteLine($"{table[1, 0]} | {table[1, 1]} | {table[1, 2]}");
+            Console.WriteLine("---------");
+            Console.WriteLine($"{table[2, 0]} | {table[2, 1]} | {table[2, 2]}");
+        }
+
+        public void SetDecision(char decision)
+        {
+            if (decision == 'X' || decision == 'x') this.decision = decisions[0];
+            else if (decision == 'O' || decision == 'o') this.decision = decisions[1];
+            else
+            {
+                Console.WriteLine("Choose only X or O");
+            }
+        }
+
+        public bool IsDecisionCorrect(char decision)
+        {
+            if (decision == 'X' || decision =='x' || decision == 'O' || decision =='o') return true;
+            else return false;
+        }
+
+        public bool IsPlaceAlreadyChecked(int key)
+        {
+
+            foreach (var item in table)
+            {
+                if (item == key+48) return false;
+            }
+            return true;
+        }
+
+        public void SetKey(int key)
+        {
+            this.key = key;
+        }
+
+        public void ChangeTable()
+        {
+            switch (this.key)
+            {
+                case 1:
+                    table[0, 0] = decision;
+                    break;
+                case 2:
+                    table[0, 1] = decision;
+                    break;
+                case 3:
+                    table[0, 2] = decision;
+                    break;
+                case 4:
+                    table[1, 0] = decision;
+                    break;
+                case 5:
+                    table[1, 1] = decision;
+                    break;
+                case 6:
+                    table[1, 2] = decision;
+                    break;
+                case 7:
+                    table[2, 0] = decision;
+                    break;
+                case 8:
+                    table[2, 1] = decision;
+                    break;
+                case 9:
+                    table[2, 2] = decision;
+                    break;
+                default:
+                    Console.WriteLine();
+                    break;
+
+            }
+        }
+
+        public bool IsXWinner()
+        {
+            if ((table[0, 0] == 'X' && table[1, 0] == 'X' && table[2, 0] == 'X') ||
+                (table[0, 0] == 'X' && table[0, 1] == 'X' && table[0, 2] == 'X') ||
+                (table[0, 0] == 'X' && table[1, 1] == 'X' && table[2, 2] == 'X') ||
+                (table[1, 0] == 'X' && table[1, 1] == 'X' && table[1, 2] == 'X') ||
+                (table[2, 0] == 'X' && table[2, 1] == 'X' && table[2, 2] == 'X') ||
+                (table[0, 1] == 'X' && table[1, 1] == 'X' && table[2, 1] == 'X') ||
+                (table[0, 2] == 'X' && table[1, 2] == 'X' && table[2, 2] == 'X'))
+                return true;
+            return false;
+        }
+        public bool IsOWinner()
+        {
+            if ((table[0, 0] == 'O' && table[1, 0] == 'O' && table[2, 0] == 'O') ||
+                (table[0, 0] == 'O' && table[0, 1] == 'O' && table[0, 2] == 'O') ||
+                (table[0, 0] == 'O' && table[1, 1] == 'O' && table[2, 2] == 'O') ||
+                (table[1, 0] == 'O' && table[1, 1] == 'O' && table[1, 2] == 'O') ||
+                (table[2, 0] == 'O' && table[2, 1] == 'O' && table[2, 2] == 'O') ||
+                (table[0, 1] == 'O' && table[1, 1] == 'O' && table[2, 1] == 'O') ||
+                (table[0, 2] == 'O' && table[1, 2] == 'O' && table[2, 2] == 'O'))
+                return true;
+            return false;
+        }
+    }
+}
